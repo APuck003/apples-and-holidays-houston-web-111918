1169231bb19dcf265d09789f55a069006dd63774
Austin Puckett <puckett_austin@yahoo.com>
2018-10-12 17:40:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,12 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |keys, values|
-    puts "#{keys}: #{values}"
-    values.each do |key, value|
-      puts "#{key}: #{value}"
-    end
-  end
+  
 
 end
 
