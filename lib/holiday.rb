c1a0a1c2f1e3859e0f8dd553752295fc149e3593
Austin Puckett <puckett_austin@yahoo.com>
2018-10-12 17:34:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
-holiday_hash[:winter].each do |k, v|
+  holiday_hash[:winter].each do |k, v|
   v.flatten
 end
 
